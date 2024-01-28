acb208f0a8c64d702429d30df80897c60e5cbfd3
SAMIAN <110727895+SAMIAN04@users.noreply.github.com>
2024-01-28 05:25:12 +0000

    Add files via upload
    All updated

--- qr.js
+++ qr.js
@@ -1,19 +1,20 @@
 const btn = document.getElementById(`btn`);
 const qr = document.getElementById(`qr`);
-const input = document.getElementById(`input`).value;
+const input = document.getElementById(`input`);
 const qrImg = document.getElementById(`qrimg`);
-const hy = document.getElementById(`hy`)
+const hy = document.getElementById(`hy`);
 btn.addEventListener(`click`, generate);
 
 function generate() {
-    if (input != ``) {
-       hy.innerHTML =  ''
-      qrImg.src= `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${input}`
-      console.log(qrImg.src);
-    }
-    else{
-        hy.innerHTML = `enter some text` 
-        qrImg.src = ``
-    }
-   
+  if (input.value == ``) {
+    hy.innerHTML = `enter some text`;
+    // qrImg.src = ``
+    // console.log(`bug`);
+  } else {
+    hy.innerHTML = "";
+    qrImg.src =
+      `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=`+input.value;
+
+    qr.classList.replace(`bg-slate-400`, `bg-gray-200`);
+  }
 }
