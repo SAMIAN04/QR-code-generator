73c91e7d42f76a255e6d421f0ce91b1526bbb4f2
SAMIAN <110727895+SAMIAN04@users.noreply.github.com>
2024-01-31 05:29:09 +0000

    Add files via upload
    Align issue fixed

--- qr.js
+++ qr.js
@@ -8,13 +8,22 @@ btn.addEventListener(`click`, generate);
 function generate() {
   if (input.value == ``) {
     hy.innerHTML = `enter some text`;
-    // qrImg.src = ``
-    // console.log(`bug`);
   } else {
     hy.innerHTML = "";
     qrImg.src =
       `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=`+input.value;
 
-    qr.classList.replace(`bg-slate-400`, `bg-gray-200`);
+    qr.classList.replace(`bg-slate-400`, `bg-white`);
+  }
+  update();
+}
+function update() {
+  if (input.value == ``) {
+    qrImg.src = ``;
+    qr.classList.replace(`bg-white`, `bg-slate-400`);
+    hy.innerHTML = `enter some text`;
   }
 }
+setInterval(() => {
+  update();
+}, 100);
